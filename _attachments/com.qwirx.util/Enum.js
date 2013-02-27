91cb78dd6a2f56fb70c307fe0750a615ebafb9c0
Chris Wilson <chris+github@qwirx.com>
2013-02-27 10:12:02 +0000

    Improve docs on how to use Enum.

--- _attachments/com.qwirx.util/Enum.js
+++ _attachments/com.qwirx.util/Enum.js
@@ -8,7 +8,13 @@ goog.provide('com.qwirx.util.Enum');
 /**
  * Convert an array to a hash whose values are the same strings
  * as the keys, which is a nice property for an enumeration, and
- * saves us having to duplicate them.
+ * saves us having to duplicate them. For example:
+ *
+ * <pre>
+com.qwirx.freebase.Freebase.Gui.OpenMode = 
+	new com.qwirx.util.Enum(['DATA', 'DESIGN']);
+assert(com.qwirx.freebase.Freebase.Gui.OpenMode.DESIGN == 'DESIGN');
+</pre>
  *
  * @see http://stackoverflow.com/a/6672823/648162
  */
